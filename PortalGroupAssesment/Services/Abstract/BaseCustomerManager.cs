0f866da8c5b60fd79f94e621f75b5bf8324992f1
fatih <fatih@DESKTOP-PT432LC>
2021-08-26 12:21:14 +0300

    Abstract base service yazıldı

---
+++ PortalGroupAssesment/Services/Abstract/BaseCustomerManager.cs
@@ -0,0 +1,16 @@
+﻿using PortalGroupAssesment.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalGroupAssesment.Services.Abstract
+{
+    public abstract class BaseCustomerManager : ICustomerService
+    {
+        public void Add(Customer customer)
+        {
+            /// db save kodları
+        }
+    }
+}
