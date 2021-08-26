b2e2f406ecc6006e8160b778cb2dc80e5b57116e
fatih <fatih@DESKTOP-PT432LC>
2021-08-26 12:00:42 +0300

    Add project files.

---
+++ PortalGroupAssesment/Entites/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalGroupAssesment.Entites
+{
+    public class Customer:IEntity
+    {
+        public int  Id  { get; set; }
+        public string TC { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+
+    }
+}
